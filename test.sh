9220b1bab9089005d5f1ae32e94d1595d79a45a1
Marko Kreen <marko.kreen@veriff.net>
2019-04-04 21:48:32 +0300

    Test selfsign cli

--- test.sh
+++ test.sh
@@ -3,11 +3,11 @@
 set -e
 
 # some pgp key that works
-pgp_me="markokr@gmail.com"
+pgp_me=$(sed -n -e '/^default-key /s/^[^ ]* //p' $HOME/.gnupg/gpg.conf)
 
 # both py2 and py3 should work
-sysca='python3.4 sysca.py'
-sysca='python sysca.py'
+sysca='python3 sysca.py'
+sysca='python2 sysca.py'
 
 ## init temp dir
 
@@ -62,7 +62,7 @@ $sysca sign \
 
 ## client cert
 
-$sysca new-key --out tmp/client.key
+$sysca new-key rsa --out tmp/client.key
 
 $sysca request \
   --key tmp/client.key \
@@ -85,3 +85,23 @@ $sysca sign \
 $sysca show tmp/client.crt \
   | sed -e '/:[0-9A-Fa-f][0-9A-Fa-f]:[0-9A-Fa-f][0-9A-Fa-f]:/d' -e '/^----/q'
 
+## selfsign
+
+$sysca new-key rsa:4096 --out tmp/selfsigned.key
+
+$sysca selfsign \
+  --key tmp/selfsigned.key \
+  --subject '/CN=client/ L = Fooza 1\/2 / SA=Läft 4\\b/' \
+  --san 'dns:*.example.com , ip : 127.0.0.1, ip:::1, uri:http://localhost/, email: me@qqq.com, dn:/T=mööTitle/' \
+  --crl-url 'http://crl0.example.com , http://crl1.example.com' \
+  --ocsp-url 'http://ocsp0.example.com , http://ocsp1.example.com' \
+  --issuer-url 'http://ftp.example.com/ca.crt, http://ftp2.example.com/ca.crt' \
+  --usage 'client, server, code, email, time, ocsp, any, content_commitment' \
+  --ocsp-nocheck \
+  --days 900 \
+  --out tmp/selfsigned.crt
+
+$sysca show tmp/selfsigned.crt \
+  | sed -e '/:[0-9A-Fa-f][0-9A-Fa-f]:[0-9A-Fa-f][0-9A-Fa-f]:/d' -e '/^----/q'
+
+echo "Success."
